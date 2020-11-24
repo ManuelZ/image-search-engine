0da43e55406c69e5215ece6b93c2bf1d852a1780
Manuel Zanutelli <mclzc89@gmail.com>
2020-11-24 10:43:47 -0500

    mark file as not used

--- MyPipeline.py
+++ MyPipeline.py
@@ -1,12 +1,19 @@
+"""
+NOT USED
+"""
 import pandas as pd
 import h5py
 import cv2
 import numpy as np
 from pathlib import Path
 import time
+from config import BATCH_SIZE
+from config import K
+from sklearn.cluster import MiniBatchKMeans
 
 extractor = cv2.BRISK_create()
 
+# This pipeline seems to be fine to pre-process images. Nothing more.
 
 class DatasetGenerator:
     """
@@ -42,21 +49,21 @@ class HDF5DatasetGenerator:
     Modified from: Pyimagesearch DL4CV
     """
     def __init__(self, dbPath, batch_size):
-		self.batch_size = batch_size
-		self.db = h5py.File(dbPath, "r")
-		self.n_images = self.db["labels"].shape[0]
+        self.batch_size = batch_size
+        self.db = h5py.File(dbPath, "r")
+        self.n_images = self.db["labels"].shape[0]
 
     def generator(self, passes=np.inf) -> list:
-		epochs = 0
-		while epochs < passes:
-			# Loop over the HDF5 dataset
-			for i in np.arange(0, self.n_images, self.batch_size):
-				images = self.db["images"][i: i + self.batch_size]
-				yield images
-			epochs += 1
+        epochs = 0
+        while epochs < passes:
+            # Loop over the HDF5 dataset
+            for i in np.arange(0, self.n_images, self.batch_size):
+                images = self.db["images"][i: i + self.batch_size]
+                yield images
+            epochs += 1
 
     def close(self):
-		self.db.close()
+        self.db.close()
 
 
 class ImagesFeatureExtractorPipeline:
@@ -99,7 +106,7 @@ class GrayTransformer:
         return [cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) for im in X]
 
 
-class FeatureDetectorDescriptorTransformer():
+class FeatureDetectorDescriptorTransformer:
     """
     Extract features and descriptors from images.
     """
@@ -118,6 +125,74 @@ class FeatureDetectorDescriptorTransformer():
         return stacked
 
 
+class Quantizer:
+    #
+    # TODO: this should not be used in this pipeline, 
+    # this pipeline applies all the processers in each batch, but this
+    # processer requires to have all the features dataset complete
+    #
+
+    def __init__(self, batch_size=500):
+        self.batch_size = batch_size
+        self.kmeans = MiniBatchKMeans(n_clusters=K,
+                             random_state=0,
+                             max_iter=500,
+                             batch_size=batch_size,
+                             verbose=0,
+                             max_no_improvement=20,
+                             )
+    def transform(self, X):
+        # Number of iterations over the whole dataset
+        n_passes = 100
+        # Max number of consecutive passes over the whole dataset without improvement
+        max_passes_no_improvement = 3
+        # Store number of failed passes over the whole dataset
+        failed_passes = 0
+        # Total number of descriptors in the whole dataset
+        n_des = X.shape[0]
+        # Inertia of the last run
+        old_inertia = 0
+
+        # 3 values average (1 / (1-beta))
+        beta = 0.6666
+
+        for i in range(n_passes):
+            np.random.shuffle(X)
+            start = 0
+            limits = np.linspace(self.batch_size, n_des, n_des//self.batch_size, dtype=int)
+            print(f'Pass {i+1} over the dataset.')
+            for end in limits:
+                self.kmeans.partial_fit(X[start:end])
+                start += self.batch_size
+            
+            inertia = self.kmeans.inertia_
+            inertia_diff = old_inertia - inertia        
+
+            if i == 0:
+                tracked_inertia = inertia
+            elif (i > 0):
+                if tracked_inertia < 0:
+                    print("Early stopping because there is no improvement.")
+                    break
+            
+            tracked_inertia = tracked_inertia * beta + (1 - beta) * inertia_diff
+            old_inertia = inertia
+            print(f'Inertia: {inertia:.0f}')
+            print(f'Inertia diff: {inertia_diff:.0f}')
+            print(f'Tracked inertia: {tracked_inertia:.0f}\n')
+
+        # K x N where N is the descriptor size
+        codebook = self.kmeans.cluster_centers_
+        
+        quantized_stacked = np.zeros(X.shape)
+        print('Building histograms...')
+        for i, (im_path, img_des) in enumerate(X):
+            # Translate a descriptor into a quantized descriptor
+            quantized_desc = codebook[kmeans.predict(img_des)]
+            quantized_stacked[i] = quantized_desc
+        return quantized_stacked
+
+
 if __name__ == '__main__':
 
     image_paths = Path("data2").rglob("*.jpg")
@@ -125,7 +200,7 @@ if __name__ == '__main__':
 
     pipeline = ImagesFeatureExtractorPipeline([
         ('to_gray', GrayTransformer()),
-        ('detect_features', FeatureDetectorDescriptorTransformer()),
+        ('detect_features', FeatureDetectorDescriptorTransformer())
     ])
 
     start = time.time()
