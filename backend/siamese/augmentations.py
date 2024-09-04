005070b5c3ba56dc47b1177f2577e399a374717a
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-09-03 19:15:52 -0500

    Add more augmentations

--- backend/siamese/augmentations.py
+++ backend/siamese/augmentations.py
@@ -1,22 +1,27 @@
 import albumentations as A
 import cv2
-from siamese.config import WHITE
+import siamese.config as config
 
 al_augmentations = A.Compose(
     [
+        A.HueSaturationValue(p=0.5),
+        A.RandomBrightnessContrast(p=0.5),
+        A.RandomGamma(gamma_limit=(60, 120), p=0.5),
+        A.ISONoise(p=0.5),
+        A.PixelDropout(p=0.5),
         A.Blur(blur_limit=5),
         A.CoarseDropout(p=0.1),
-        # Zoom
+        # Zoom out only
         A.ShiftScaleRotate(
             shift_limit=0,
             rotate_limit=0,
-            scale_limit=(-0.2, 0),  # Zoom out only
+            scale_limit=(-0.1, 0),  # Zoom out only
             border_mode=cv2.BORDER_CONSTANT,
-            value=WHITE,
-            p=1.0,
+            value=config.WHITE,
+            p=0.5,
         ),
         A.Perspective(
-            fit_output=True, pad_mode=cv2.BORDER_CONSTANT, pad_val=WHITE, p=0.3
+            fit_output=True, pad_mode=cv2.BORDER_CONSTANT, pad_val=config.WHITE, p=0.3
         ),
         # Shift only
         A.ShiftScaleRotate(
@@ -24,10 +29,12 @@ al_augmentations = A.Compose(
             rotate_limit=0,
             scale_limit=0,
             border_mode=cv2.BORDER_CONSTANT,
-            value=WHITE,
+            value=config.WHITE,
             p=0.5,
         ),
-        A.SafeRotate(limit=10, border_mode=cv2.BORDER_CONSTANT, value=WHITE, p=0.1),
-        A.OpticalDistortion(border_mode=cv2.BORDER_CONSTANT, value=WHITE),
+        A.SafeRotate(
+            limit=10, border_mode=cv2.BORDER_CONSTANT, value=config.WHITE, p=0.1
+        ),
+        A.OpticalDistortion(border_mode=cv2.BORDER_CONSTANT, value=config.WHITE),
     ]
 )
